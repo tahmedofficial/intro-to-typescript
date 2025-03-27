654f9b72407a440db6b00c46e05d22af1f03cbbe
tahmedofficial <tanvirofcuo@gmail.com>
2025-03-27 16:10:57 +0600

    learning abstract class

---
+++ abstractClass.ts
@@ -0,0 +1,27 @@
+abstract class TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+    ) { }
+
+    abstract getSepia(): void
+    getReelTime(): number {
+        return 8
+    }
+}
+
+class Instagram extends TakePhoto {
+    constructor(
+        public cameraMode: string,
+        public filter: string,
+        public burst: number
+    ) {
+        super(cameraMode, filter)
+    }
+
+    getSepia(): void {
+
+    }
+}
+
+const tanvir = new Instagram("test", "test", 8)
