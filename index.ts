8bec121e9512d0905ae7866dd1e01d269ee401e6
tahmedofficial <tanvirofcuo@gmail.com>
2025-03-27 15:15:40 +0600

    learning get and set

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 // class User {
 //     public email: string;
-//     private name: string;
+//     public name: string;
 //     readonly city: string = "Dhaka";
 //     constructor(email: string, name: string) {
 //         this.email = email;
@@ -9,15 +9,39 @@
 // }
 
 class User {
+    private _courseCount: number = 1;
     readonly city: string = "Dhaka";
     constructor(
         public email: string,
-        public name: string
-    ) {
+        public name: string,
+    ) { }
+
+    get getAppleEmail(): string {
+        return `apple ${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more then 1")
+        }
+        this._courseCount = courseNum;
     }
 }
 
 const tanvir = new User("tanvir@ahmed.com", "Tanvir Ahmed")
+
+// class User {
+//     readonly city: string = "Dhaka";
+//     constructor(
+//         public email: string,
+//         public name: string
+//     ) { }
+// }
+
 // tanvir.name
 
 export { }
