d6625c230ed3f3a3654b892c6e1b4563b49e17e9
tahmedofficial <tanvirofcuo@gmail.com>
2025-05-06 21:17:13 +0600

    learning modifier

---
+++ module-3/3.5.ts
@@ -0,0 +1,45 @@
+{
+
+    class BankAccount {
+        readonly id: number;
+        name: string;
+        protected _balance: number;
+
+        constructor(id: number, name: string, balance: number) {
+            this.id = id;
+            this.name = name;
+            this._balance = balance
+        }
+
+        addDeposite(amount: number) {
+            this._balance = this._balance + amount;
+        }
+
+        getBalance() {
+            return this._balance;
+        }
+    }
+
+    class StudentAccount extends BankAccount {
+        test() {
+            this._balance
+        }
+    }
+
+    const goribAcccount = new BankAccount(242, "Gorib", 20);
+    goribAcccount.addDeposite(400);
+    goribAcccount.getBalance();
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
